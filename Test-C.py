0bb85747c34ae4ba4c449a1f6084cf58712887fb
Bit77 <daren@markandtraci.com>
2025-08-08 15:35:42 -0500

    Update Test-C.py

--- Test-C.py
+++ Test-C.py
@@ -1,1 +1,16 @@
+python3 -c "
+import time
 
+def fib(n):
+    if n <= 1:
+        return n
+    return fib(n-1) + fib(n-2)
+
+start = time.time()
+runs = 0
+while time.time() - start < 10:
+    fib(20)
+    runs += 1
+
+print(f'Completed {runs} Fibonacci(20) calculations in 10s')
+"
