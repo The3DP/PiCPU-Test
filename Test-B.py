694a55df03960004f68205a5d52e7603b2401dbe
Bit77 <daren@markandtraci.com>
2025-08-08 15:35:19 -0500

    Update Test-B.py

--- Test-B.py
+++ Test-B.py
@@ -1,1 +1,18 @@
+python3 -c "
+import threading, time
 
+def stress():
+    x = 0
+    while time.time() < end_time:
+        x += 1
+
+num_threads = 4
+duration = 10
+end_time = time.time() + duration
+
+threads = [threading.Thread(target=stress) for _ in range(num_threads)]
+[t.start() for t in threads]
+[t.join() for t in threads]
+
+print(f'Multithreaded stress test completed for {duration} seconds')
+"
