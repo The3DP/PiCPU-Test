8f44b938778b4d071badc3793a86b0e6c4782001
Bit77 <daren@markandtraci.com>
2025-08-08 15:36:05 -0500

    Update Test-D.py

--- Test-D.py
+++ Test-D.py
@@ -1,1 +1,10 @@
+python3 -c "
+import time, math
+start = time.time()
+ops = 0
+while time.time() - start < 10:
+    math.sqrt(ops % 1000)
+    ops += 1
 
+print(f'Floating-point operations in 10s: {ops}')
+"
